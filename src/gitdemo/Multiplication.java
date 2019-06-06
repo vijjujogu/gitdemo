5312372c768f7a52456b0c53f6a5f584f16e37d1
vijayalakshmi <vijayalakshmi.jogu@atmecs.com>
2019-06-06 17:53:48 +0530

    Branch1

---
+++ src/gitdemo/Multiplication.java
@@ -0,0 +1,13 @@
+package gitdemo;
+
+public class Multiplication {
+	
+		public static void main(String[] args) {
+		int n=2;
+		for(int i=1;i<=5;i++){  
+				     System.out.println(n*i);  
+				      
+				   }  
+		}		  
+		}
+
