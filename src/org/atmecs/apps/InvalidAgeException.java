3a8df0500f08ca68dbec348694f8b1878d2b1c4a
hp <hp@hi>
2019-06-30 22:43:41 +0530

    exception

---
+++ src/org/atmecs/apps/InvalidAgeException.java
@@ -0,0 +1,33 @@
+package org.atmecs.apps;
+
+public class InvalidAgeException extends RuntimeException {
+
+	
+	private int age;
+	
+	public InvalidAgeException() {
+		super();
+	}
+	public InvalidAgeException(int age) {
+		this.age = age;
+	}
+	public InvalidAgeException(String msg, int age) {
+		super(msg);
+		this.age = age;
+	}
+	public InvalidAgeException(String msg, Throwable cause, int age) {
+		super(msg, cause);
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "InvalidAgeException for Age: " + getAge();
+	}
+	@Override
+	public String getMessage() {
+		return "InvalidAgeException for Age: " + getAge();
+	}
+	public int getAge() {
+		return age;
+	}
+}
